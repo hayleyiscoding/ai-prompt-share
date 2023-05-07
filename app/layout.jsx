55f838d32654348c7b4a25a217f6203bc3fc1428
Hayley <hayleyiscoding@gmail.com>
2023-05-07 15:59:30 +1000

    project setup, mongoDB, next-auth

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "@styles/global.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "AI Prompt",
+  description: "Share and discover the best AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
